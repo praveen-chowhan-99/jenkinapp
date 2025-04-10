ed6511402ec186363b290662f8159f09fc3e021a
praveen-chowhan-99 <praveenkorra.office@gmail.com>
2025-04-10 09:36:32 +0530

    jenkin.java added

---
+++ Jenkin.java
@@ -0,0 +1,6 @@
+public class Jenkin {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! from HelloWorrld.java");
+        System.out.println("jenkin code from HelloWorrld.java");
+    }
+}
